a29e5bea0974b22b450257c0d5babaf65ce7156b
Kurva2076 <kotplombir@gmail.com>
2025-10-06 06:07:31 +0300

    ЛБ-1: Добавление аннотации для лучшего документирования

--- src/TimeUtils.java
+++ src/TimeUtils.java
@@ -1,29 +1,31 @@
+import org.jetbrains.annotations.NotNull;
+
 public class TimeUtils {
     public static final Integer DAYS_PER_MONTH = 30;
     public static final Integer DAYS_PER_YEAR = 30 * 12;
     public static final Integer MONTHS_PER_YEAR = 12;
 
-    public static Integer convertDaysIntoMonths(Integer days) {
+    public static @NotNull Integer convertDaysIntoMonths(@NotNull Integer days) {
         return days / DAYS_PER_MONTH;
     }
 
-    public static Integer convertDaysIntoYears(Integer days) {
+    public static @NotNull Integer convertDaysIntoYears(@NotNull Integer days) {
         return days / DAYS_PER_YEAR;
     }
 
-    public static Integer convertMonthsIntoYears(Integer months) {
+    public static @NotNull Integer convertMonthsIntoYears(@NotNull Integer months) {
         return months / MONTHS_PER_YEAR;
     }
 
-    public static Integer convertYearsIntoMonths(Integer years) {
+    public static @NotNull Integer convertYearsIntoMonths(@NotNull Integer years) {
         return years * MONTHS_PER_YEAR;
     }
 
-    public static Integer convertYearsIntoDays(Integer years) {
+    public static @NotNull Integer convertYearsIntoDays(@NotNull Integer years) {
         return years * DAYS_PER_YEAR;
     }
 
-    public static Integer convertMonthsIntoDays(Integer months) {
+    public static @NotNull Integer convertMonthsIntoDays(@NotNull Integer months) {
         return months * MONTHS_PER_YEAR;
     }
 }
